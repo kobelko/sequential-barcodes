60865c3a2258217f3ce989374966e5dcf133d903
Samuel Kobelkowsky <sk@kobelsoft.com>
2022-02-02 11:21:06 -0600

    Added project files

---
+++ config.sample.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Sample configuration fil
+ * @author Samuel Kobelkowsky
+ * @copyright 2022 (c) Samuel Kobelkowsky
+ * @version 0.1
+ */
+
+define('RECAPTCHA_SITEKEY', 'YOUR_SITE_KEY_HERE' );
+define('RECAPTCHA_SECRET', 'YOUR_SECRET_HERE');
